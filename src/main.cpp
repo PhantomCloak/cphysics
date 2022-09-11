1338c5d74e5ffc209fd9763cd86ffb4307ed58b5
PhantomCloak <phantom_clk@protonmail.com>
2022-09-12 02:55:45 +0300

    format: main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,17 @@
-#include <stdio.h>
-#include <SDL2/SDL.h>
 #include "Application.h"
+#include <SDL2/SDL.h>
+#include <stdio.h>
 
-int main (int argc, char *argv[])
-{
-    Application app;
-    app.Setup();
+int main(int argc, char *argv[]) {
+  Application app;
+  app.Setup();
 
-    while(app.IsRunning())
-    {
-        app.Input();
-        app.Update();
-        app.Render();
-    }
+  while (app.IsRunning()) {
+    app.Input();
+    app.Update();
+    app.Render();
+  }
 
-    app.Destroy();
-    return 0;
+  app.Destroy();
+  return 0;
 }
