2d130876b96885e487bbced0e0e69d8e8cc4ab63
PhantomCloak <phantom_clk@protonmail.com>
2022-09-12 03:12:55 +0300

    format: Application.cpp

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,51 +1,35 @@
 #include "Application.h"
 
-void Application::Setup()
-{
-    appRunning = Graphics::OpenWindow();
-}
+void Application::Setup() { appRunning = Graphics::OpenWindow(); }
 
-bool Application::IsRunning()
-{
-    return appRunning;
-}
-void Application::Input()
-{
-    SDL_Event event;
-    while (SDL_PollEvent(&event))
-    {
-        switch (event.type) {
-            case SDL_QUIT:
-                appRunning = false;
-                break;
-            case SDL_KEYDOWN:
-                if (event.key.keysym.sym == SDLK_ESCAPE)
-                    appRunning = false;
-                break;
-        }
+bool Application::IsRunning() { return appRunning; }
+void Application::Input() {
+  SDL_Event event;
+  while (SDL_PollEvent(&event)) {
+    switch (event.type) {
+    case SDL_QUIT:
+      appRunning = false;
+      break;
+    case SDL_KEYDOWN:
+      if (event.key.keysym.sym == SDLK_ESCAPE)
+        appRunning = false;
+      break;
     }
+  }
 }
 
-void Application::Update()
-{
-    Graphics::ClearScreen(0xFFFFFFFF);
+void Application::Update() {
+  static int timePreviousFrame;
+  int timeToWait = MILLISECS_PER_FRAME - (SDL_GetTicks() - timePreviousFrame);
+  if (timeToWait > 0)
+    SDL_Delay(timeToWait);
 
-    static int timePreviousFrame;
-    int timeToWait = MILLISECS_PER_FRAME - (SDL_GetTicks() - timePreviousFrame);
-    if (timeToWait > 0)
-        SDL_Delay(timeToWait);
+  Graphics::DrawCircle(500, 500, 40, 0, 0xffff0000);
 
-    Graphics::DrawCircle(500,500,40,0,0xffff0000);
-
-    timePreviousFrame = SDL_GetTicks();
+  timePreviousFrame = SDL_GetTicks();
+  Graphics::ClearScreen(0xFFFFFFFF);
 }
 
-void Application::Render()
-{
-    Graphics::RenderFrame();
-}
+void Application::Render() { Graphics::RenderFrame(); }
 
-void Application::Destroy()
-{
-
-}
+void Application::Destroy() {}
