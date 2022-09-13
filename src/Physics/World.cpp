158b7709ba4b0986178beef6d2f26f6e33692110
PhantomCloak <phantom_clk@protonmail.com>
2022-09-13 12:23:05 +0300

    add: world.cpp

---
+++ src/Physics/World.cpp
@@ -0,0 +1,11 @@
+#include "World.h"
+
+World::World(float g) { gravity = g; }
+
+World::~World() {}
+
+void World::AddForce(const Vector2 &force) {}
+
+void World::AddTorque(const Vector2 &force) {}
+
+void World::Update(float dt) {}
