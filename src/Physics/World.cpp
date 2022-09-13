7e112d23009cc4262f11dc5272ba1f1c5d79f535
PhantomCloak <phantom_clk@protonmail.com>
2022-09-13 19:07:45 +0300

    add: particle funcs to world

--- src/Physics/World.cpp
+++ src/Physics/World.cpp
@@ -1,11 +1,29 @@
 #include "World.h"
 
-World::World(float g) { gravity = g; }
+World::World(float g) { gravity = -g; }
 
-World::~World() {}
+void World::AddParticle(Particle *particle) { particles.push_back(particle); }
 
 void World::AddForce(const Vector2 &force) {}
 
 void World::AddTorque(const Vector2 &force) {}
 
-void World::Update(float dt) {}
+std::vector<Particle *> &World::GetParticles() { return particles; }
+
+void World::Update(float dt) {
+
+  // Add all forces
+  const Vector2 gravityForce = Vector2(0, gravity);
+
+  for (auto particle : particles) {
+    particle->AddForce(gravityForce);
+  }
+
+  // Integrate all forces
+  for (auto particle : particles) {
+    if (particle->mass != 0)
+      particle->Integrate(dt);
+  }
+}
+
+void World::CheckCollisions() {}
--- src/Physics/World.h
+++ src/Physics/World.h
@@ -1,6 +1,7 @@
 #ifndef WORLD_H
 #define WORLD_H
 
+#include "Particle.h"
 #include "Vector2.h"
 #include <vector>
 
@@ -8,13 +9,18 @@ class World {
 private:
   float gravity = -9.8;
   std::vector<float> torques;
+  std::vector<Particle *> particles;
 
 public:
-  World(float gravity);
+  World(float g);
   ~World();
+
+  void AddParticle(Particle *p);
 
   void AddForce(const Vector2 &force);
   void AddTorque(const Vector2 &force);
+
+  std::vector<Particle *> &GetParticles();
 
   void Update(float dt);
   void CheckCollisions();
