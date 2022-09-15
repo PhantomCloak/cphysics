de4e2030d8e642ec1170103dc0b511ca19a07245
PhantomCloak <phantom_clk@protonmail.com>
2022-09-16 00:58:28 +0300

    add: simple rotating circles to scene

--- src/Application.cpp
+++ src/Application.cpp
@@ -14,17 +14,28 @@ void Application::Input() {
       appRunning = false;
       break;
     case SDL_MOUSEBUTTONDOWN:
-      if (event.button.button == SDL_BUTTON_LEFT) {
-        int x, y;
-        SDL_GetMouseState(&x, &y);
+      int x, y;
+      SDL_GetMouseState(&x, &y);
 
-        Particle *particle = new Particle(x, y, 1);
+      if (event.button.button == SDL_BUTTON_LEFT) {
+
+        int randomMass = rand() % 5;
+        randomMass = randomMass == 0 ? 1 : randomMass;
+
+        Particle *particle = new Particle(x, y, randomMass);
         world->AddParticle(particle);
+      } else if (event.button.button == SDL_BUTTON_RIGHT) {
+        Circle *circle = new Circle(x, y, 25, 5);
+        world->AddCircle(circle);
       }
       break;
     case SDL_KEYDOWN:
       if (event.key.keysym.sym == SDLK_ESCAPE)
         appRunning = false;
+      if (event.key.keysym.sym == SDLK_a)
+        for (auto c : world->GetCircles()) {
+          c->AddTorque(15);
+        }
       break;
     }
   }
@@ -45,11 +56,16 @@ void Application::Update() {
 }
 
 void Application::Render() {
-
   Graphics::ClearScreen(0xffffffff);
+
   for (auto particle : world->GetParticles()) {
-    Graphics::DrawFillCircle(particle->position.x, particle->position.y, 20,
+    Graphics::DrawFillCircle(particle->position.x, particle->position.y, 2,
                              0xff000000);
+  }
+
+  for (auto circle : world->GetCircles()) {
+    Graphics::DrawCircle(circle->position.x, circle->position.y, circle->radius,
+                         circle->rotation, 0xff000000);
   }
 
   Graphics::RenderFrame();
